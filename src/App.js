6c086225fe934cc2750d4f376545601fbd700982
pmtejas <thejas.chintu@gmail.com>
2023-07-16 18:27:49 +0530

    Payment

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
 import React, { useEffect } from "react";
-import './App.css'
-import Header from './Header'
-import Home from './Home'
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './Login';
+import Login from "./Login";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
 import Checkout from "./Checkout";
 import Payment from "./Payment";
-
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+const promise = loadStripe(
+  "pk_test_51M0sctSF7KQwLGgsKgoEKeQDCqzZNFPMwwNNWGdH8C96wgxOFwW5mfx4qy27iW2mcYFVxoyjs5Qjk1kTvW4dfCLv00CujIYHrv"
+);
 function App() {
   //eslint-disable-next-line
   const [{}, dispatch] = useStateValue();
-  useEffect(()=>{
-    auth.onAuthStateChanged((authuser)=>{
-      console.log("THE USER IS >>>>",authuser);
+  useEffect(() => {
+    auth.onAuthStateChanged((authuser) => {
+      console.log("THE USER IS >>>>", authuser);
       if (authuser) {
         dispatch({
           type: "SET_USER",
           user: authuser,
-        })
+        });
       } else {
         dispatch({
-          type:"SET_USER",
-          user:null,
-        })
-        
+          type: "SET_USER",
+          user: null,
+        });
       }
-    })
+    });
     //eslint-disable-next-line
-  },[]);
+  }, []);
   return (
     <BrowserRouter>
       <div className="app">
@@ -43,7 +46,7 @@ function App() {
               </>
             }
           />
-             <Route
+          <Route
             path="/login"
             element={
               <>
@@ -52,28 +55,29 @@ function App() {
             }
           />
           <Route
-          path="/checkout"
-          element={
-            <>
-            <Header />
-            <Checkout/>
-            </>
-          }
+            path="/checkout"
+            element={
+              <>
+                <Header />
+                <Checkout />
+              </>
+            }
           />
           <Route
-          path="/payment"
-          element={
-            <>
-            <Header />
-            <Payment/>
-            </>
-          }
+            path="/payment"
+            element={
+              <>
+                <Header />
+                <Elements stripe={promise}>
+                  <Payment />
+                </Elements>
+              </>
+            }
           />
-
         </Routes>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Payment.js
+++ src/Payment.js
@@ -1,9 +1,87 @@
-import React from 'react'
+import React, { useState } from "react";
 import "./Payment.css";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
+import { Link } from "react-router-dom";
+import { getBasketTotal } from "./reducer";
+import { CardElement } from "@stripe/react-stripe-js";
+
 function Payment() {
+  //eslint-disable-next-line
+  const [{ basket, user }, dispatch] = useStateValue();
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+  const orderTotal = getBasketTotal(basket);
+  // Format the order total value using Intl.NumberFormat
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "INR",
+  });
+  const handleChange = (event) => {
+    // Listen for changes in the CardElement
+    // and display any errors as the customer types their card details
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : "");
+  };
   return (
-    <div>Payment</div>
-  )
+    <div className="payment">
+      <div className="payment__container">
+        <h1>
+          Checkout (<Link to="/checkout">{basket?.length} items</Link>)
+        </h1>
+
+        {/* payment__section-delivery address */}
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Delivery Address</h3>
+          </div>
+          <div className="payment__address">
+            <p>{user?.email}</p>
+            <p>Tumakuru,Karnataka</p>
+            <p>India</p>
+          </div>
+        </div>
+
+        {/* payment__section-Review items */}
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className="payment__items">
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        </div>
+
+        {/* payment__section-PAyment method */}
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details">
+            {/* stripe magic will go */}
+            <form >
+            <CardElement onChange={handleChange} />
+              <div className="payment__priceContainer">
+               <h3>Order Total: {formatter.format(orderTotal)}</h3>
+                <button disabled={disabled}>
+                  <span>Buy Now</span>
+                </button>
+              </div>
+              {error && <div>{error}</div>}
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Payment
+export default Payment;
